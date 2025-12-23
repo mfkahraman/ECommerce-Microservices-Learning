4cda58ffd14acd9822f2a15d1b69ae5a53a17597
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-12-23 16:42:28 +0300

    Add generic MongoDB repository and interface for entities
    Introduced a generic repository pattern for MongoDB by adding GenericRepository<TEntity> and IRepository<TEntity> in the Ecommerce.Catalog.Repositories namespace. These provide reusable, asynchronous CRUD operations for any entity inheriting from BaseEntity, promoting code reuse and separation of concerns.

---
+++ Services/Catalog/Ecommerce.Catalog/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Ecommerce.Catalog.Entities.Common;
+using Ecommerce.Catalog.Settings;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+
+namespace Ecommerce.Catalog.Repositories
+{
+    public class GenericRepository<TEntity> 
+        : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly IMongoCollection<TEntity> _collection;
+
+        public GenericRepository(IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _collection = database.GetCollection<TEntity>(typeof(TEntity).Name.ToLowerInvariant());
+        }
+        public async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            await _collection.InsertOneAsync(entity, cancellationToken: cancellationToken);
+        }
+
+        public async Task DeleteAsync(string id, CancellationToken cancellationToken = default)
+        {
+            await _collection.FindOneAndDeleteAsync
+                (x=> x.Id == id, cancellationToken: cancellationToken);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _collection.AsQueryable().ToListAsync(cancellationToken);
+        }
+
+        public async Task<TEntity?> GetByIdAsync(string id, CancellationToken cancellationToken = default)
+        {
+            return await _collection.Find(x => x.Id == id)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            await _collection.FindOneAndReplaceAsync
+                (x => x.Id == entity.Id, entity, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ Services/Catalog/Ecommerce.Catalog/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Catalog.Entities.Common;
+using ZstdSharp.Unsafe;
+
+namespace Ecommerce.Catalog.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<TEntity?> GetByIdAsync(string id, CancellationToken cancellationToken = default);
+        Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(string id, CancellationToken cancellationToken = default);
+    }
+}
