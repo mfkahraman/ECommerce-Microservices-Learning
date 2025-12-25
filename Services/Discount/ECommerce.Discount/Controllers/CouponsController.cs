18d45b924afa5b34d58480caad11e20ddd244930
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-12-25 23:51:49 +0300

    Add endpoints to get coupon by ID and create new coupon
    Added GET endpoint to retrieve a coupon by its ID, returning 404 if not found. Introduced POST endpoint to create a new coupon using CreateCouponDto, returning 201 Created with the new resource location. Also made minor formatting improvements to the GetAll method.

--- Services/Discount/ECommerce.Discount/Controllers/CouponsController.cs
+++ Services/Discount/ECommerce.Discount/Controllers/CouponsController.cs
@@ -1,5 +1,6 @@
 ﻿using ECommerce.Discount.Context;
 using ECommerce.Discount.DTOs;
+using ECommerce.Discount.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,9 +14,9 @@ namespace ECommerce.Discount.Controllers
         public async Task<IActionResult> GetAll()
         {
             var records = await context.Coupons.ToListAsync();
-            var values = records.Select(x=> new ResultCouponDto
+            var values = records.Select(x => new ResultCouponDto
             {
-               CouponId = x.CouponId,
+                CouponId = x.CouponId,
                 Code = x.Code,
                 DiscountRate = x.DiscountRate,
                 ExpireDate = x.ExpireDate,
@@ -23,6 +24,38 @@ namespace ECommerce.Discount.Controllers
             }).ToList();
 
             return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var record = await context.Coupons.FindAsync(id);
+            if (record == null)
+                return NotFound();
+            var value = new ResultCouponDto
+            {
+                CouponId = record.CouponId,
+                Code = record.Code,
+                DiscountRate = record.DiscountRate,
+                ExpireDate = record.ExpireDate,
+                ProductId = record.ProductId
+            };
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCouponDto dto)
+        {
+            var entity = new Coupon
+            {
+                Code = dto.Code,
+                DiscountRate = dto.DiscountRate,
+                ExpireDate = dto.ExpireDate,
+                ProductId = dto.ProductId
+            };
+            context.Coupons.Add(entity);
+            await context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = entity.CouponId }, entity);
         }
     }
 }
